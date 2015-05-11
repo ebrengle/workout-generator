6b622836a589c809427b043e5662e2cefce023e9
Eric Brengle <eric.brengle@gmail.com>
2015-05-11 10:18:04 -0500

    setting up test boiler plate

---
+++ test/integration/test_adding_a_new_exercise.rb
@@ -0,0 +1,21 @@
+require_relative '../test_helper'
+
+##Add an exercise:
+In order to keep all my workouts straight I want to create a workout and add it to my list.
+
+
+# Usage Example:
+# > ./workout-generator
+# 1. Add a workout
+# 2. List all exercises
+# 3. Create Workout
+# 4. Exit
+
+# > 1
+# Please Enter the Name of Your First Exercise:
+# > Push Ups
+# Please Enter the number of reps:
+# > 15
+# Would you like to Add another exercise? (y/n)
+# > n
+# Exit
